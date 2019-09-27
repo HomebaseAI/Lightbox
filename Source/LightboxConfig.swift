7d49690f5f6571e9ba14058b83cc8000bbfb2c2b
Reagan Wood <reaganwood1@gmail.com>
2019-09-27 10:25:38 -0500

    remove image url loading from Lightbox because it doesn't meet our use case

--- Source/LightboxConfig.swift
+++ Source/LightboxConfig.swift
@@ -1,7 +1,6 @@
 import UIKit
 import AVKit
 import AVFoundation
-import Imaginary
 
 public class LightboxConfig {
   /// Whether to show status bar while Lightbox is presented
@@ -15,20 +14,6 @@ public class LightboxConfig {
     from.present(videoController, animated: true) {
       videoController.player?.play()
     }
-  }
-
-  /// How to load image onto UIImageView
-  public static var loadImage: (UIImageView, URL, ((UIImage?) -> Void)?) -> Void = { (imageView, imageURL, completion) in
-
-    // Use Imaginary by default
-    imageView.setImage(url: imageURL, placeholder: nil, completion: { result in
-      switch result {
-      case .value(let image):
-        completion?(image)
-      case .error:
-        completion?(nil)
-      }
-    })
   }
 
   /// Indicator is used to show while image is being fetched
